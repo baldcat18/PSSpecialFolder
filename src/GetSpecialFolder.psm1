a612073fd90ffc387c295469ebebb54cbfad0aa8
baldcat18 <baldcat@outlook.jp>
2018-12-14 22:06:35 +0900

    基本的なclassや関数を定義

---
+++ src/GetSpecialFolder.psm1
@@ -0,0 +1,20 @@
+﻿Set-StrictMode -Version Latest
+
+class SpecialFolder {
+	[string]$Title
+	[string]$Path
+	
+	hidden [string]$Dir
+	hidden [__ComObject]$FolderItem
+}
+
+class FolderOption {
+	[string]$Title
+	[string]$Path
+}
+
+function newSpecialFolder {
+	[OutputType([SpecialFolder])]
+	param ([string]$Dir, [FolderOption]$Option = (@{}))
+	
+}

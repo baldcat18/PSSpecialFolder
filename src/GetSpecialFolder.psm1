f8d5532cfef27ecc8b8bd6b597704cf08d8a9832
baldcat18 <baldcat@outlook.jp>
2018-12-22 21:37:02 +0900

    newSpecialFolderをshellプロトコルに対応させた

--- src/GetSpecialFolder.psm1
+++ src/GetSpecialFolder.psm1
@@ -13,8 +13,35 @@ class FolderOption {
 	[string]$Path
 }
 
+$shell = New-Object -ComObject Shell.Application
+
 function newSpecialFolder {
 	[OutputType([SpecialFolder])]
 	param ([string]$Dir, [FolderOption]$Option = (@{}))
 	
+	if (!$Dir) { return }
+	if ($Dir.Substring(0, 2) -eq "\\") { $Dir = "file:" + $Dir }
+	elseif ($Dir.Substring(0, 6) -ne "shell:" -and $Dir.Substring(0, 5) -ne "file:") { $Dir = "file:\\\" + $Dir }
+	
+	try { $folder = $shell.NameSpace($Dir) }
+	catch { return }
+	
+	if (!$folder) { return }
+	$folderItem = $shell.NameSpace($Dir).Self
+	
+	$title =
+		if ($Dir -match "^shell:(?:(?:\w|\s)+)$") { $Dir.Substring(6) }
+		elseif ($Dir -match "^shell:::.*\{\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\}$") {
+			$clsid = $Dir.Substring($Dir.Length - 38)
+			(Get-Item "Microsoft.PowerShell.Core\Registry::HKEY_CLASSES_ROOT\CLSID\$clsid").GetValue("")
+		}
+		else { $Dir -replace "^.+\\(.+?)$", "`$1" }
+	
+	$path = $folderItem.Path
+	if ($path.Substring(0,2) -eq "::") { $path = "shell:" + $path }
+	
+	if ($Option.Title) { $title = $Option.Title }
+	if ($Option.Path) { $path = $Option.Path }
+	
+	return [SpecialFolder]@{ Title = $title; Path = $path; Dir = $Dir; FolderItem = $folderItem }
 }
---
+++ tests/GetSpecialFolder.Tests.ps1
@@ -0,0 +1,59 @@
+﻿Import-Module "$PSScriptRoot/../src/GetSpecialFolder.psm1" -Force
+
+Describe "newSpecialFolder のテスト" {
+	Context "Null" {
+		It "Should Null" {
+			newSpecialFolder $null | Should -BeFalse
+		}
+	}
+	Context "shell:Foo" {
+		It "Should Null" {
+			newSpecialFolder "shell:Foo" | Should -BeFalse
+		}
+	}
+	Context "shell:::{00000000-0000-0000-0000-000000000000}" {
+		It "Should Null" {
+			newSpecialFolder "shell:::{00000000-0000-0000-0000-000000000000}" | Should -BeFalse
+		}
+	}
+	Context "shell:Desktop" {
+		$folder = newSpecialFolder "shell:Desktop"
+		It 'Title should "Desktop"' {
+			$folder.Title | Should -Be "Desktop"
+		}
+		$desktopPath = [Environment]::GetFolderPath([Environment+SpecialFolder]::Desktop)
+		It "Path should `"$desktopPath`"" {
+			$folder.Path | Should -Be $desktopPath
+		}
+	}
+	Context "shell:MyComputerFolder" {
+		$folder = newSpecialFolder "shell:MyComputerFolder"
+		It 'Title should "MyComputerFolder"' {
+			$folder.Title | Should -Be "MyComputerFolder"
+		}
+		$thisPcPath = "shell:::{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+		It "Path should `"$thisPcPath`"" {
+			$folder.Path | Should -Be $thisPcPath
+		}
+	}
+	$recycleBinPath = 'shell:::{645FF040-5081-101B-9F08-00AA002F954E}'
+	Context "$recycleBinPath (Recycle Bin)" {
+		$folder = newSpecialFolder $recycleBinPath
+		It 'Title should "Recycle Bin"' {
+			$folder.Title | Should -Be "Recycle Bin"
+		}
+		It "Path should `"$recycleBinPath`"" {
+			$folder.Path | Should -Be $recycleBinPath
+		}
+	}
+	$controlPanelPath = 'shell:::{26EE0668-A00A-44D7-9371-BEB064C98683}\0'
+	Context "$controlPanelPath (All Control Panel Items)" {
+		$folder = newSpecialFolder $controlPanelPath
+		It 'Title should "0"' {
+			$folder.Title | Should -Be '0'
+		}
+		It "Path should `"$controlPanelPath`"" {
+			$folder.Path | Should -Be $controlPanelPath
+		}
+	}
+}

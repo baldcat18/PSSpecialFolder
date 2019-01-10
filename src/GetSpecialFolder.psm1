a670d5d30edbf60ed2aecc1d2278bbddeb724a40
baldcat18 <baldcat@outlook.jp>
2019-01-10 21:35:35 +0900

    OtherShellCommandsカテゴリを実装

--- src/GetSpecialFolder.psm1
+++ src/GetSpecialFolder.psm1
@@ -188,4 +188,104 @@ function Get-SpecialFolder {
 	Write-Output (newSpecialFolder $(if ($win10) { "shell:OneDrivePictures" } else { "shell:SkyDrivePictures" }))
 	# shell:OneDrive\Pictures\Camera Roll
 	Write-Output (newSpecialFolder $(if ($win10) { "shell:OneDriveCameraRoll" } else { "shell:SkyDriveCameraRoll" }))
+	
+	Write-Information "Category: OtherShellCommands`n"
+	
+	# Taskbar
+	Write-Output (newShellCommand "shell:::{0DF44EAA-FF21-4412-828E-260A8728E7F1}")
+	# Search
+	# Win10 1511まで
+	Write-Output (newShellCommand "shell:::{2559A1F0-21D7-11D4-BDAF-00C04F60B9F0}")
+	# Help and Support
+	# Win8.1まで
+	Write-Output (newShellCommand "shell:::{2559A1F1-21D7-11D4-BDAF-00C04F60B9F0}")
+	# Run...
+	Write-Output (newShellCommand "shell:::{2559A1F3-21D7-11D4-BDAF-00C04F60B9F0}")
+	# E-mail
+	Write-Output (newShellCommand "shell:::{2559A1F5-21D7-11D4-BDAF-00C04F60B9F0}")
+	# Set Program Access and Defaults
+	Write-Output (newShellCommand "shell:::{2559A1F7-21D7-11D4-BDAF-00C04F60B9F0}")
+	# Win8から
+	Write-Output (newShellCommand "shell:::{2559A1F8-21D7-11D4-BDAF-00C04F60B9F0}" $(if ($win10) { "Cortana" } else { "Search" }))
+	# Show Desktop
+	# Win+Dと同じ
+	Write-Output (newShellCommand "shell:::{3080F90D-D7AD-11D9-BD98-0000947B0257}")
+	# Window Switcher
+	# Win7ではCtrl+Win+Tab、Win8/8.1ではCtrl+Alt+Tab、Win10 1607以降ではWin+Tabと同じ (Win10 1507/1511では使用不可)
+	Write-Output (newShellCommand "shell:::{3080F90E-D7AD-11D9-BD98-0000947B0257}")
+	# Windows Sidebar Properties
+	# Win7まで
+	Write-Output (newShellCommand "shell:::{37EFD44D-EF8D-41B1-940D-96973A50E9E0}")
+	Write-Output (newShellCommand "shell:::{38A98528-6CBF-4CA9-8DC0-B1E1D10F7B1B}" "Connect To")
+	# Phone and Modem Control Panel
+	Write-Output (newShellCommand "shell:::{40419485-C444-4567-851A-2DD7BFA1684D}")
+	# Open in new window
+	# Win8.1から
+	Write-Output (newShellCommand "shell:::{52205FD8-5DFB-447D-801A-D0B52F2E83E1}" "File Explorer")
+	# Mobility Center Control Panel
+	Write-Output (newShellCommand "shell:::{5EA4F148-308C-46D7-98A9-49041B1DD468}")
+	# Region and Language
+	Write-Output (newShellCommand "shell:::{62D8ED13-C9D0-4CE8-A914-47DD628FB1B0}")
+	# Windows Features
+	Write-Output (newShellCommand "shell:::{67718415-C450-4F3C-BF8A-B487642DC39B}")
+	# Mouse Control Panel
+	Write-Output (newShellCommand "shell:::{6C8EEC18-8D75-41B2-A177-8831D59D2D50}")
+	# Folder Options
+	Write-Output (newShellCommand "shell:::{6DFD7C5C-2451-11D3-A299-00C04F8EF6AF}")
+	# Keyboard Control Panel
+	Write-Output (newShellCommand "shell:::{725BE8F7-668E-4C7B-8F90-46BDB0936430}")
+	# Device Manager
+	Write-Output (newShellCommand "shell:::{74246BFC-4C96-11D0-ABEF-0020AF6B0B7A}")
+	# CardSpace
+	# Win8まで
+	Write-Output (newShellCommand "shell:::{78CB147A-98EA-4AA6-B0DF-C8681F69341C}")
+	# User Accounts
+	# netplwiz.exe / control.exe userpasswords2
+	Write-Output (newShellCommand "shell:::{7A9D77BD-5403-11D2-8785-2E0420524153}")
+	# Tablet PC Settings Control Panel
+	Write-Output (newShellCommand "shell:::{80F3F1D5-FECA-45F3-BC32-752C152E456E}")
+	# Internet Folder
+	# Win10以降では開けないので非表示に
+	Write-Output $(if (!$win10) { newShellCommand "shell:InternetFolder" })
+	# Indexing Options Control Panel
+	Write-Output (newShellCommand "shell:::{87D66A43-7B11-4A28-9811-C86EE395ACF7}")
+	# Portable Workspace Creator
+	# Win8から
+	# Enterpriseで使用可
+	# Win10 1607以降ではProでも使用可
+	Write-Output (newShellCommand "shell:::{8E0C279D-0BD1-43C3-9EBD-31C3DC5B8A77}")
+	# Biometrics navigate target object (Welcome to Biometric Devices)
+	# Win8まで
+	Write-Output (newShellCommand "shell:::{8E35B548-F174-4C7D-81E2-8ED33126F6FD}")
+	# Infrared
+	# Win10 1607から
+	Write-Output (newShellCommand "shell:::{A0275511-0E86-4ECA-97C2-ECD8F1221D08}")
+	# Internet Options
+	Write-Output (newShellCommand "shell:::{A3DD4F92-658A-410F-84FD-6FBBBEF2FFFE}")
+	# Color Management
+	Write-Output (newShellCommand "shell:::{B2C761C6-29BC-4F19-9251-E6195265BAF1}")
+	# Windows Anytime Upgrade
+	# Win8.1まで
+	Write-Output (newShellCommand "shell:::{BE122A0E-4503-11DA-8BDE-F66BAD1E3F3A}")
+	# Biometrics navigate target object (Biometric Devices\Message)
+	# Win8まで
+	# shell:::{CCFB7955-B4DC-42CE-893D-884D72DD6B19}
+	Write-Output (newShellCommand "shell:::{CBC84B69-69EA-439B-B791-DF15F60333CF}")
+	# Text to Speech Control Panel
+	Write-Output (newShellCommand "shell:::{D17D1D6D-CC3F-4815-8FE3-607E7D5D10B3}")
+	# Add Network Place
+	Write-Output (newShellCommand "shell:::{D4480A50-BA28-11D1-8E75-00C04FA31A86}")
+	# Windows Defender
+	# Win10 1607まで
+	Write-Output (newShellCommand "shell:::{D8559EB9-20C0-410E-BEDA-7ED416AECC2A}")
+	# Date and Time Control Panel
+	Write-Output (newShellCommand "shell:::{E2E7934B-DCE5-43C4-9576-7FE4F75E7480}")
+	# Sound Control Panel
+	Write-Output (newShellCommand "shell:::{F2DDFC82-8F12-4CDD-B7DC-D4FE1425AA4D}")
+	# Pen and Touch Control Panel
+	Write-Output (newShellCommand "shell:::{F82DF8F7-8B9F-442E-A48C-818EA735FF9B}")
+	
+	<#
+	Write-Output (newSpecialFolder "shell:")
+	#>
 }

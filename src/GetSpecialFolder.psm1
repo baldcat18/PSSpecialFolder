5e8fcbef75c3cf00a1e3837d1238a34af3f5e6a6
baldcat18 <baldcat@outlook.jp>
2019-02-11 22:09:32 +0900

    サポート対象外の環境では例外や警告を出すようにした

--- src/GetSpecialFolder.psm1
+++ src/GetSpecialFolder.psm1
@@ -1,5 +1,10 @@
 ﻿Set-StrictMode -Version Latest
 
+if ($Host.Version -ge [version]::new(6, 0) -and !$IsWindows) {
+	throw [PlatformNotSupportedException]::new("The PSSpecialFolder module supports Windows only.")
+	return
+}
+
 class SpecialFolder {
 	[string]$Title
 	[string]$Path
@@ -17,6 +22,16 @@ function const {
 	param ([string]$name, $value)
 	
 	New-Variable -Name $name -Value $value -Option Constant -Scope 1
+}
+
+const osVersion ([Environment]::OSVersion.Version)
+# Win10以降
+const win10 ($osVersion -gt [version]::new(10, 0))
+# Win10 1607以降
+const win10_1607 ($osVersion -gt [version]::new(10, 0, 14393))
+
+if ($win10 -and !$win10_1607) {
+	Write-Warning "The PSSpecialFolder module supports Windows 10 Version 1607+."
 }
 
 const shell (New-Object -ComObject Shell.Application)
@@ -77,20 +92,14 @@ function Get-SpecialFolder {
 	[OutputType([SpecialFolder[]])]
 	param ()
 	
-	const osVerion ([Environment]::OSVersion.Version)
-	
 	# Win8.1以降
-	const win81 ($osVerion -gt [Version]::new(6, 3))
-	# Win10以降
-	const win10 ($osVerion -gt [Version]::new(10, 0))
-	# Win10 1607以降
-	const win10_1607 ($osVerion -gt [Version]::new(10, 0, 14393))
+	const win81 ($osVersion -gt [version]::new(6, 3))
 	# Win10 1703以降
-	const win10_1703 ($osVerion -gt [Version]::new(10, 0, 15063))
+	const win10_1703 ($osVersion -gt [version]::new(10, 0, 15063))
 	# Win10 1709以降
-	const win10_1709 ($osVerion -gt [Version]::new(10, 0, 16299))
+	const win10_1709 ($osVersion -gt [version]::new(10, 0, 16299))
 	# Win10 1803以降
-	const win10_1803 ($osVerion -gt [Version]::new(10, 0, 17134))
+	const win10_1803 ($osVersion -gt [version]::new(10, 0, 17134))
 	
 	const is64bitOS ([Environment]::Is64BitOperatingSystem)
 	const isWow64 ($is64bitOS -and ![Environment]::Is64BitProcess)

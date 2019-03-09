cbfc53e545916d462b2076904f7991b446740d4e
baldcat18 <baldcat@outlook.jp>
2019-03-10 08:47:31 +0900

    tools/Test-Project.ps1で実行するテストを追加

--- tools/Test-Project.ps1
+++ tools/Test-Project.ps1
@@ -1,1 +1,18 @@
-﻿Invoke-ScriptAnalyzer -Path $PSScriptRoot/.. -Recurse
+﻿<#
+.SYNOPSIS
+PSSpecialFolderプロジェクトに対するテストを実行する
+#>
+
+[CmdletBinding()]
+param()
+
+Push-Location $PSScriptRoot/..
+
+Write-Output "Test-ModuleManifest:"
+Test-ModuleManifest src/PSSpecialFolder.psd1
+Write-Output "`nInvoke-ScriptAnalyzer:"
+Invoke-ScriptAnalyzer -Path . -Recurse
+Write-Output "`nInvoke-Pester:"
+Invoke-Pester tests
+
+Pop-Location

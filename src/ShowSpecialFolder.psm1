37c46ac7cbc5df7dd08ef14150bbabd006f89c73
baldcat18 <baldcat@outlook.jp>
2019-06-15 22:32:34 +0900

    WPFが使えないときの処理を追加

--- src/ShowSpecialFolder.psm1
+++ src/ShowSpecialFolder.psm1
@@ -2,7 +2,15 @@
 
 function Show-SpecialFolder {
 	[CmdletBinding()]
-	param ()
+	param ([switch]$IncludeShellCommand)
 	
-	throw [NotImplementedException]::new()
+	# WPFが使えない場合
+	if (($PSVersionTable['PSVersion'].Major -ge 6) -or ($Host.Runspace.ApartmentState -ne "STA")) {
+		powershell.exe -Sta -Command "Show-SpecialFolder -IncludeShellCommand:`$$IncludeShellCommand"
+		return
+	}
+	
+	# 今のところは単にGet-SpecialFolderを呼ぶだけ
+	# Show-SpecialFolderでは情報ストリームを使わないので非表示に
+	Get-SpecialFolder -IncludeShellCommand:$IncludeShellCommand -InformationAction SilentlyContinue
 }
